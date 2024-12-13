7926d0b436aa82c4a059700b727fb758994525e3
benitomartin <bmartinc80@gmail.com>
2024-12-13 22:47:46 +0100

    News Signal

---
+++ services/news-signal/llms/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AnthropicConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='anthropic_credentials.env',
+        env_file_encoding='utf-8',
+    )
+    llm_name: str
+    api_key: str
+
+# config = AnthropicConfig()
+
+class OllamaConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file='ollama.env')
+    llm_name: str
